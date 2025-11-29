b90f14a6fbff9710e7632708c70b443c766fc7ad
sarahandressa <sarahagdiaz@yahoo.com.br>
2025-11-29 18:04:48 -0300

    W03 Project: Project 2 Part 1 (CRUD Operations) -  last update

--- swagger.js
+++ swagger.js
@@ -1,8 +1,26 @@
 const swaggerUi = require('swagger-ui-express');
-const swaggerFile = require('./swagger-output.json');
+const fs = require('fs');
+const path = require('path');
 
 function setupSwagger(app) {
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+  const swaggerFile = path.join(__dirname, 'swagger-output.json');
+
+
+  if (!fs.existsSync(swaggerFile)) {
+    console.warn('⚠️  swagger-output.json not found. Run: npm run generate-swagger');
+    return;
+  }
+
+  const swaggerData = JSON.parse(fs.readFileSync(swaggerFile, 'utf8'));
+
+  
+  const renderURL = 'https://cse341-project-2-1.onrender.com';
+
+  swaggerData.host = renderURL.replace('https://', '');
+  swaggerData.schemes = ['https'];
+  swaggerData.basePath = '';
+
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerData));
 }
 
 module.exports = setupSwagger;
