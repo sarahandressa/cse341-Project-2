425fe53082bf9a74e083449e73991a5204f265da
sarahandressa <sarahagdiaz@yahoo.com.br>
2025-11-29 20:00:24 -0300

    W03 Project: Project 2 Part 1 (CRUD Operations) - update

--- swagger.js
+++ swagger.js
@@ -1,26 +1,23 @@
+const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
-const fs = require('fs');
-const path = require('path');
 
-function setupSwagger(app) {
-  const swaggerFile = path.join(__dirname, 'swagger-output.json');
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Book Club API',
+      version: '1.0.0',
+      description: 'API to manage books'
+    },
+    servers: [
+      { url: 'https://cse341-project-2-1.onrender.com' } 
+    ],
+  },
+  apis: ['./routes/*.js'], 
+};
 
+const swaggerSpec = swaggerJsdoc(options);
 
-  if (!fs.existsSync(swaggerFile)) {
-    console.warn('⚠️  swagger-output.json not found. Run: npm run generate-swagger');
-    return;
-  }
-
-  const swaggerData = JSON.parse(fs.readFileSync(swaggerFile, 'utf8'));
-
-  
-  const renderURL = 'https://cse341-project-2-1.onrender.com';
-
-  swaggerData.host = renderURL.replace('https://', '');
-  swaggerData.schemes = ['https'];
-  swaggerData.basePath = '';
-
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerData));
-}
-
-module.exports = setupSwagger;
+module.exports = (app) => {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+};
